4012b7d868bb1caa79a908a9d5536e998f098d2a
James Prior <jep200404@columbus.rr.com>
2016-08-31 11:43:09 -0400

    james-prior/circle2.py: Modest struture cleanup

--- james-prior/circle2.py
+++ james-prior/circle2.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 
-from tkinter import Tk, Canvas, PhotoImage, mainloop
-from math import sin
-from time import sleep
 from sys import argv
+from tkinter import Tk, Canvas, PhotoImage, mainloop
 
 WIDTH, HEIGHT = 640, 480
 
-CORNA = int(argv[1])
-CORNB = int(argv[2])
-SIDE = float(argv[3])
 
-window = Tk()
-canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
-canvas.pack()
-img = PhotoImage(width=WIDTH, height=HEIGHT)
-canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
+def main(argv):
+    corna = int(argv[1])
+    cornb = int(argv[2])
+    side = float(argv[3])
 
-for i in range(WIDTH):
-    for j in range(HEIGHT):
-        x = CORNA + (SIDE * (i/100.0))
-        y = CORNB + (SIDE * (j/100.0))
-        z = x*x + y*y
-        c = int(z)
-        if c % 2 == 0:
-            img.put("#ffffff", (i,j))
+    window = Tk()
+    canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
+    canvas.pack()
+    img = PhotoImage(width=WIDTH, height=HEIGHT)
+    canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
-mainloop()
+    for i in range(WIDTH):
+        for j in range(HEIGHT):
+            x = corna + (side * (i/100.0))
+            y = cornb + (side * (j/100.0))
+            z = x*x + y*y
+            c = int(z)
+            if c % 2 == 0:
+                img.put("#ffffff", (i,j))
+
+    mainloop()
+
+
+if __name__ == '__main__':
+    main(argv)

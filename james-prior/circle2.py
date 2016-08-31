373ce903304aa0f3fef6c7e8302f6899dd36bff8
James Prior <jep200404@columbus.rr.com>
2016-08-31 11:50:50 -0400

    james-prior/circle2.py: Reorganized for speed. 0.826s
    -15 -20 87 0.826s
    -15 -20 9 0.814s
    -4506 21539 3.12 0.813s

--- james-prior/circle2.py
+++ james-prior/circle2.py
@@ -6,6 +6,15 @@ from tkinter import Tk, Canvas, PhotoImage, mainloop
 WIDTH, HEIGHT = 640, 480
 
 
+def color(xx, yy):
+    z = xx + yy
+    c = int(z)
+    if c % 2 == 0:
+        return '#ffffff'
+    else:
+        return '#000000'
+
+
 def main(argv):
     corna = int(argv[1])
     cornb = int(argv[2])
@@ -17,14 +26,21 @@ def main(argv):
     img = PhotoImage(width=WIDTH, height=HEIGHT)
     canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
-    for i in range(WIDTH):
-        for j in range(HEIGHT):
-            x = corna + (side * (i/100.0))
-            y = cornb + (side * (j/100.0))
-            z = x*x + y*y
-            c = int(z)
-            if c % 2 == 0:
-                img.put("#ffffff", (i,j))
+    x_squareds = [x*x for x in [
+        corna + (side * (i/100.0))
+        for i in range(WIDTH)]]
+
+    y_squareds = [y*y for y in [
+        cornb + (side * (j/100.0))
+        for j in range(HEIGHT)]]
+
+    lines = []
+    for yy in y_squareds:
+        horizontal_line = '{' + ' '.join(
+            color(xx, yy)
+            for xx in x_squareds) + '}'
+        lines.append(horizontal_line)
+    img.put(' '.join(lines))
 
     mainloop()
 

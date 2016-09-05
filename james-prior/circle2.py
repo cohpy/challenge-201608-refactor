8c242cf8dadeb494061419be1adf69d581b3db5f
James Prior <jep200404@columbus.rr.com>
2016-09-05 12:15:35 -0400

    james-prior/circle2.py: Change to pixel based units. Allow expressions. Begin docstring.

--- james-prior/circle2.py
+++ james-prior/circle2.py
@@ -1,7 +1,49 @@
 #!/usr/bin/env python3
+'''
+arguments may be expressions
+
+    available values:
+        width (unit is 1 pixel)
+        height (unit is 1 pixel)
+        pi
+        e
+        math.*
+
+circle2.py 0 0 .17
+circle2.py 0 0 .34
+circle2.py 0 0 .8
+circle2.py 0 0 .81
+circle2.py 0 0 .801
+circle2.py 0 0 .8001
+circle2.py 0 0 .80001
+circle2.py 0 0 .9
+circle2.py 0 0 .99
+circle2.py 0 0 .999
+circle2.py 0 0 .9999
+circle2.py 0 0 .99999
+circle2.py 0 0 .999999
+circle2.py 0 0 1
+circle2.py 0 0 1.1
+circle2.py 0 0 1.01
+circle2.py 0 0 1.001
+circle2.py 0 0 1.0001
+circle2.py 0 0 1.00001
+circle2.py 0 0 1.000001
+circle2.py 0 0 1.0000001
+circle2.py 0 0 1/3
+circle2.py 0 0 1/width
+circle2.py 0 0 1/pi
+circle2.py 0 0 1/e
+circle2.py 0 0 'math.sqrt(1/e)'
+circle2.py 0 0 'math.sqrt(1/2)'
+circle2.py 0 0 'math.sqrt(1/pi)'
+circle2.py width/2 height/2  'e/(min(width, height)/2)'
+'''
 
 from sys import argv
 from tkinter import Tk, Canvas, PhotoImage, mainloop
+from math import pi, e
+import math
 
 BLACK = '#000000'
 RED = '#ff0000'
@@ -28,26 +70,18 @@ def color(xx, yy):
 
 
 def main(argv):
-    corna = int(argv[1])
-    cornb = int(argv[2])
-    side = float(argv[3])
-
     window = Tk()
     screen_size = window.winfo_screenwidth(), window.winfo_screenheight()
     width, height = screen_size
+    corner_x, corner_y, m = map(eval, argv[1:])
     # print(screen_size)
     canvas = Canvas(window, width=width, height=height, bg=BACKGROUND_COLOR)
     canvas.pack()
     img = PhotoImage(width=width, height=height)
     canvas.create_image((width/2, height/2), image=img, state="normal")
 
-    x_squareds = [x*x for x in [
-        corna + (side * (i/100.0))
-        for i in range(width)]]
-
-    y_squareds = [y*y for y in [
-        cornb + (side * (j/100.0))
-        for j in range(height)]]
+    x_squareds = [(m * (x-corner_x))**2 for x in range(width)]
+    y_squareds = [(m * (y-corner_y))**2 for y in range(height)]
 
     lines = []
     for yy in y_squareds:

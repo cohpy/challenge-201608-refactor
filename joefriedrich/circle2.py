2f758f5cb71bca8be507a2c3acf0afc4ad5b9c4c
Joe Friedrich <joe.friedrich@gmail.com>
2016-09-25 23:45:14 -0400

    Final Commit
    Generators
    Variable window size
    Better UI
    The notebook is where I tested the code.
    
    Need to add:
    Limit on window Width and Height based on system settings!!
    Testing!!

--- joefriedrich/circle2.py
+++ joefriedrich/circle2.py
@@ -3,11 +3,46 @@ from math import sin
 from time import sleep
 from sys import argv
 
-WIDTH, HEIGHT = 640, 480
+def get_int_input():
+    while True:
+        try:
+            return int(input('Please enter a whole number:  '))
+        except ValueError:
+            print('Invalid input.  Please try again.')
 
-CORNA = int(argv[1])
-CORNB = int(argv[2])
-SIDE = float(argv[3])
+def get_float_input():
+    while True:
+        try:
+            return float(input('Please enter a floating point number:  '))
+        except ValueError:
+            print('Invalid input.  Please try again.')
+
+def generate_width(column, row, user):
+    while column > 0:
+        for pixel in generate_height(column, row, user):
+            yield column, pixel
+        column -= 1
+
+def generate_height(column, row, user):
+    while row > 0:
+        x = user[0] + (user[2] * (column/100.0))
+        y = user[1] + (user[2] * (row/100.0))
+        z = x*x + y*y
+        if int(z) % 2 == 0:
+            yield row
+        row -= 1
+
+print('How wide would you like the output?')
+WIDTH = get_int_input()
+print('How tall would you like the output?')
+HEIGHT = get_int_input()
+print('The following two numbers should blah blah blah geometry.')
+CORNA = get_int_input()
+CORNB = get_int_input()
+print("[I hope this isn't getting tedious...]")
+SIDE = get_float_input()
+
+user = [CORNA, CORNB, SIDE]
 
 window = Tk()
 canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
@@ -15,12 +50,7 @@ canvas.pack()
 img = PhotoImage(width=WIDTH, height=HEIGHT)
 canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
-for column in range(WIDTH):
-    for row in range(HEIGHT):
-        x = CORNA + (SIDE * (column/100.0))
-        y = CORNB + (SIDE * (row/100.0))
-        z = x*x + y*y
-        if int(z) % 2 == 0:
-            img.put("#ffffff", (column,row))
+for column, row in generate_width(WIDTH, HEIGHT, user):
+    img.put("#ffffff", (column,row))
 
 mainloop()
